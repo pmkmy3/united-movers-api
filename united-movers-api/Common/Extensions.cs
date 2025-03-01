c098b6b1c78017706d16ce13358d89213075638e
pmkmy3 <pmkmy3@gmail.com>
2025-03-02 00:11:18 +0530

    added extension method for Date Format

---
+++ united-movers-api/Common/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace united_movers_api.Common
+{
+    public static class Extensions
+    {
+        public static DateTime ToCustomFormattedDate(this DateTime dateTime)
+        {
+            string formattedDate = dateTime.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
+            return DateTime.ParseExact(formattedDate, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+        }
+    }
+}
