c1fdd1f9da7909a90f11f0ccab69042acec9218d
Eeshanth <Eeshanth@DESKTOP-9GSROVN>
2025-03-02 14:34:59 +0530

    ValidateAndCreateEmployeeIDRequest

---
+++ united-movers-api/Models/ValidateAndCreateEmployeeIDRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace united_movers_api.Models
+{
+    public class ValidateAndCreateEmployeeIDRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string AadhaarNumber { get; set; }
+        public string PAN { get; set; }
+        public string ContactNumber { get; set; }
+        public string BloodGroup { get; set; }
+        public string PersonalEmailID { get; set; }
+        public int LoggedInUserID { get; set; }
+    }
+}
